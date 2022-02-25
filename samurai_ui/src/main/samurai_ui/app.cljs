bd50b874ac85edee79252fac4576605ae1e5157d
spicyfalafel <285574@niuitmo.ru>
2022-02-25 05:39:24 +0300

    start of mui frontend

--- samurai_ui/src/main/samurai_ui/app.cljs
+++ samurai_ui/src/main/samurai_ui/app.cljs
@@ -1,10 +1,17 @@
 (ns samurai-ui.app
-  (:require [reagent.core :as r])
-  (:require [reagent.dom :as dom])
-  (:require [samurai-ui.requests :as myreq])
-  (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [cljs.core.async :refer [<!]] [cljs-http.client :as http])
-  )
+  (:require [reagent.core :as r]
+            [reagent.dom :as dom]
+            [samurai-ui.requests :as myreq]
+            ["@material-ui/core/Button" :default Button]
+            ["@material-ui/core/TextField" :default TextField]
+            ["@material-ui/core/Container" :default Container]
+            ["@material-ui/core/Box" :default Box]
+            ["@material-ui/core/Card" :default Card]
+            ["@material-ui/core/CardActions" :default CardActions]
+            ["@material-ui/core/CardContent" :default CardContent]))
+
+
+
 
 
 (defn init []
@@ -13,21 +20,44 @@
 
 
 (defn Application []
-  [:div.my-container
-   [:h1.app-name "Patients"]
-   [:button.add-patient-btn {:onClick (fn [] (myreq/get-patients))} "Add patient"]
+  [:> Container {:maxWidth :xl}
+   [:> Box {:sx {:bgcolor "#cfe8fc"}}
+    [:h1.app-name "Patients"]
+    [:form.add-patient-form
 
-   (doall
-    (for [p @myreq/patients-atom]
-      [:div.card
-       [:h2 "Name: " (:firstname p) " "(:lastname p)]
-       [:h2 "Gender: " (:gender p)]
-       [:h2 "Birthdate: " (:birthdate p)]
-       [:h2 "Address: " (:address p)]
-       [:h2 "Polys: " (:polys_id p)]
-       [:button {:onClick (fn [] (myreq/get-patients))} "Remove patient"]]
-      ))
-   ])
+     [:h2 "Add patient"]
+     [:> Button {:onClick (fn [] (js/alert "not now"))
+                 :color :primary :variant :contained} "Submit"]
+     [:div
+      [:> TextField {:variant :outlined
+                     :label "Firstname"}]]
+     [:div
+       [:> TextField {:variant :outlined
+                      :label "Secondname"}]]
+     [:div
+       [:> TextField {:variant :outlined
+                      :label "Gender"}]]
+     [:div
+      [:> TextField {:variant :outlined
+                     :label "Address"}]]
+     [:div
+       [:> TextField {:variant :outlined
+                      :label "Polys number"}]]]
+
+    (doall
+     (for [p @myreq/patients-atom]
+       [:> Box {:sx {:maxWidth 200}}
+        [:> Card
+         [:> CardContent
+          [:> TextField {:label "Firstname"
+                         :size :small :variant :filled
+                         :fullWidth true
+                         :defaultValue (:firstname p)}]
+          [:> TextField {:label "Lastname" :defaultValue (:lastname p)}]
+          [:> TextField {:label "Birthdate" :defaultValue (:birthdate p)}]
+          [:> TextField {:label "Gender" :defaultValue (:gender p)}]
+          [:> TextField {:label "Addressname" :defaultValue (:address p)}]
+          [:> TextField {:label "Polys" :defaultValue (:polys p)}]]]]))]])
+
 
 (dom/render [Application] (js/document.getElementById "app"))
-
